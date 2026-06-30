84bb3a59e03907ce55e43b429d038322dab64bed
Preston Lamb <preston.j.lamb@gmail.com>
2026-06-29 23:12:57 -0600

    revert(debugging): revert tablet CORS and dynamic IP configs

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -10,26 +10,8 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
 
-  const isProd = process.env['NODE_ENV'] === 'production';
   app.enableCors({
-    origin: (origin, callback) => {
-      if (
-        !origin ||
-        !isProd ||
-        origin.startsWith('http://localhost') ||
-        origin.startsWith('http://127.0.0.1') ||
-        origin.match(/^http:\/\/192\.168\.\d+\.\d+/) ||
-        origin.match(/^http:\/\/10\.\d+\.\d+\.\d+/) ||
-        origin.match(/^http:\/\/172\.(1[6-9]|2\d|3[0-1])\.\d+\.\d+/) ||
-        origin.endsWith('.local') ||
-        origin.match(/\.local:\d+$/) ||
-        origin === 'https://apex.prestonlamb.us'
-      ) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: ['http://localhost:8200', 'https://apex.prestonlamb.us'],
     credentials: true,
   });
 
